ba8c79dbaab1abf60ff2f70e3c6922a6afd5459c
Benjamin <benjamin.clay01@gmail.com>
2025-09-20 16:22:25 -0600

    Added outlines to dock windows, made widgets scale properly

--- machinimaker/machinimaker.cpp
+++ machinimaker/machinimaker.cpp
@@ -3,22 +3,26 @@
 
 #include <QApplication>
 #include <QStyle>
-#include <QVBoxLayout>
+#include <QLayout>
+
 
 machinimaker::machinimaker(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::machinimaker)
 {
     ui->setupUi(this);
-    QVBoxLayout *pLayout = new QVBoxLayout(this);
 
-    pLayout->setSpacing(0);
-    pLayout->setContentsMargins(0, 0, 0, 0);
+    ui->toolBar->setStyleSheet("background: #ebedfa; border-bottom: 2px groove #6c6c6c");
+    ui->Viewport->setStyleSheet("background-color: #00FF00;");
+    ui->DialogBox->setStyleSheet("background-color: rgb(240, 240, 240);");
+    ui->Debugger->setStyleSheet("color: rgb(0, 0, 0); background-color: rgb(240, 240, 240);");
+    qApp->setStyleSheet("QDockWidget::title{background: #c8c8c8; color: #000000} QTextEdit{border:1px groove #848484} QMainWindow::separator { width: 1px; height: 0px; }");
 
-    setLayout(pLayout);
+
 }
-
 machinimaker::~machinimaker()
 {
     delete ui;
 }
+
+
